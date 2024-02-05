3cd3f25afec4a092229c2874681b6cdecc8c2bc9
axon4 <65050809+axon4@users.noreply.github.com>
2024-02-05 18:29:24 +0000

    write forecast-fetcher--function from Weather-API

---
+++ services/weatherAPI/forecast.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+async function fetchForeCast(location) {
+	return await axios({
+		method: 'GET',
+		url: 'https://api.weatherapi.com/v1/forecast.json',
+		params: {
+			key: process.env.WEATHER_API__KEY,
+			q: location,
+			days: 3
+		},
+		responseType: 'json'
+	})
+		.then(response => {
+			const city = response.data.location.name;
+			const region = response.data.location.region;
+			const country = response.data.location.country;
+			const locationName = [city, region, country].join(', ');
+
+			const forecast = response.data.forecast.forecastday.map(foreCastDay => ({
+				date: foreCastDay.date,
+				temperatureMinimumC: foreCastDay.day.mintemp_c,
+				temperatureMaximumC: foreCastDay.day.maxtemp_c,
+				temperatureMinimumF: foreCastDay.day.mintemp_f,
+				temperatureMaximumF: foreCastDay.day.maxtemp_f
+			}));
+
+			return { location: locationName, forecast };
+		})
+		.catch(console.error);
+};
+
+module.exports = { fetchForeCast };
